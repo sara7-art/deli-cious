947fea7d88ae8adcad1c0239edd68143d6bd7a8a
Sara Yoda <yodasara700@gmail.com>
2026-05-28 11:32:13 -0700

    BLT EXTENDS SANDWICH(ADDED CUSTOM CLASS BLT ELEMENTS)

--- src/main/java/com/pluralsight/signature/BLT.java
+++ src/main/java/com/pluralsight/signature/BLT.java
@@ -1,4 +1,28 @@
 package com.pluralsight.signature;
 
-public class BLT {
+import com.pluralsight.model.Sandwich;
+import com.pluralsight.model.Topping;
+
+public class BLT extends Sandwich {
+
+    // CONSTRUCTOR
+
+    public BLT() {
+
+         // 8 inch
+         // white bread
+         // toasted
+
+        super(8, "White", true);
+
+        // ADD DEFAULT TOPPINGS
+
+        addTopping(new Topping("Bacon", "meat", false));
+
+        addTopping(new Topping("Cheddar", "cheese", false));
+
+        addTopping(new Topping("Lettuce", "regular", false));
+
+        addTopping(new Topping("Tomato", "regular", false));
+    }
 }
