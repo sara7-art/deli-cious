5a75dd35f41b92179992e911cd2ec8dbf564e366
Sara Yoda <yodasara700@gmail.com>
2026-05-28 14:12:13 -0700

    PhillyCheeseSteak EXTENDS SANDWICH(ADDED CUSTOM CLASS PhillyCheeseSteak ELEMENTS)

--- src/main/java/com/pluralsight/signature/PhillyCheeseSteak.java
+++ src/main/java/com/pluralsight/signature/PhillyCheeseSteak.java
@@ -1,4 +1,23 @@
 package com.pluralsight.signature;
 
-public class PhillyCheeseSteak {
+import com.pluralsight.model.Sandwich;
+import com.pluralsight.model.Topping;
+
+public class PhillyCheeseSteak extends Sandwich {
+
+    // CONSTRUCTOR
+    public PhillyCheeseSteak(){
+        // 8inch
+        // White bread
+        // Toasted
+        super(8, "White", true);
+
+        // Default Toppings
+        addTopping(new Topping("Steak", "meat", false));
+
+        addTopping(new Topping("American Cheese", "cheese", false));
+
+        addTopping(new Topping("Peppers", "regular", false));
+    }
 }
+
